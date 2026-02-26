457a341c9f048e4de71076288f0050578d9894b9
fivan <fivan841@gmail.com>
2026-02-26 19:04:13 +0200

    feat: git init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ToDo from "./components/ToDo";
+import { TasksProvider } from "./context/TasksContext";
+
+const App = () => {
+  return (
+    <>
+      <TasksProvider>
+        <ToDo />
+      </TasksProvider>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,22 @@
+const Button = (props) => {
+  const {
+    className = "",
+    type = "button",
+    children,
+    onClick,
+    isDisabled,
+  } = props;
+
+  return (
+    <button
+      className={`button ${className}`}
+      type={type}
+      disabled={isDisabled}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
